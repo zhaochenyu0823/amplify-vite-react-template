f6d6cee8373ca7a5bb49f1038d5b7fd368274e76
WEL-ADNET\C_ZHAO <Chenyu_Zhao-p@wellnet.co.jp>
2024-05-14 10:13:57 +0900

    Test

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+
+import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
@@ -36,6 +38,13 @@ function App() {
             </li>
         ))}
       </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://next-release-dev.d1ywzrxfkb9wgg.amplifyapp.com/react/start/quickstart/vite-react-app/#step-2-add-delete-to-do-functionality">
+          Review next step of this tutorial.
+        </a>
+      </div>
     </main>
   );
 }
