5cc495b3dada7254099b858e12ddcd8f235c3316
WEL-ADNET\C_ZHAO <Chenyu_Zhao-p@wellnet.co.jp>
2024-05-21 09:49:14 +0900

    New Table Customer Collection Test

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,12 +7,39 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
+  Todo: a.model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+      name:a.string(),
+    }),
+    Customer: a
+      .model({
+        customerId: a.id().required(),
+        // fields can be of various scalar types,
+        // such as string, boolean, float, integers etc.
+        name: a.string(),
+        // fields can be of custom types
+        location: a.customType({
+          // fields can be required or optional
+          lat: a.float().required(),
+          long: a.float().required(),
+        }),
+        // fields can be enums
+        engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
+        collectionId: a.id(),
+        collection: a.belongsTo("Collection", "collectionId")
+        // Use custom identifiers. By default, it uses an `id: a.id()` field
+      }).identifier(["customerId"]),
+
+      Collection: a
+      .model({
+        customers: a.hasMany("Customer", "collectionId"), // setup relationships between types
+        tags: a.string().array(), // fields can be arrays
+        representativeId: a.id().required(),
+        // customize secondary indexes to optimize your query performance
+      })
+      .secondaryIndexes((index) => [index("representativeId")]),
+
+}).authorization((allow) => [allow.publicApiKey()]);
 
 export type Schema = ClientSchema<typeof schema>;
 
